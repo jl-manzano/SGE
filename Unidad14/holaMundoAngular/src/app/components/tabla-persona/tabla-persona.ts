e9cea3a0a0cca6495328df565aa2ba31d69e523b
jl-manzano <jl.manzano@iesnervion.es>
2026-01-13 12:12:52 +0100

    commit

---
+++ Unidad14/holaMundoAngular/src/app/components/tabla-persona/tabla-persona.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-persona',
+  imports: [],
+  templateUrl: './tabla-persona.html',
+  styleUrl: './tabla-persona.css',
+})
+export class TablaPersona {
+
+}
